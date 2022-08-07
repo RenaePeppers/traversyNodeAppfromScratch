5d60e2919144f4b7a74aa365794e8b557222382c
RenaePeppers <93863815+RenaePeppers@users.noreply.github.com>
2022-08-06 21:27:37 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router()
+const {ensureAuth, ensureGuest} = require('../middleware/auth')
+
+// description Login/Landing page
+//@route GET /
+router.get('/', ensureGuest, (req, res) => {
+    res.render('login', 
+        {layout: 'login',
+    })
+})
+
+
+// description Dashboard
+//@route GET /dashboard
+router.get('/dashboard', ensureAuth, (req, res) => {
+    console.log(req.User)
+    res.render('dashboard')
+})
+
+module.exports=router
